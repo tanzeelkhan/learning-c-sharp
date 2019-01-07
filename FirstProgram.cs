8d65f3d4c43a9eb3dda40201374181e95e719ff4
Tanzeel Khan <tanzeelkhan94@gmail.com>
2019-01-07 13:11:38 +0530

    Create FirstProgram.cs

---
+++ FirstProgram.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            //This is a comment
+        }
+    }
+}
